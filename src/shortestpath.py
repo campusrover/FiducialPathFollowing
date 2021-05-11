59dfefb24e7be2c5045728cc7c3f9bc6db9c0982
Jacqueline <jianingzhou@brandeis.edu>
2021-05-11 03:15:59 +0000

    update

--- src/shortestpath.py
+++ src/shortestpath.py
@@ -18,11 +18,11 @@ def get_neighbors(graph,row,col):
     neighbors=[]
     #ensure neighbors are within image boundaries
     if row > 0 and not graph[row - 1][col].processed:
-         neighbors.append(graph[r-1][c])
-    if row < shape[0] - 1 and not graph[row + 1][c].processed:
-            neighbors.append(graph[row + 1][c])
+         neighbors.append(graph[row-1][col])
+    if row < shape[0] - 1 and not graph[row + 1][col].processed:
+            neighbors.append(graph[row + 1][col])
     if col > 0 and not graph[row][col-1].processed:
-        neighbors.append(graph[row][c-1])
+        neighbors.append(graph[row][col-1])
     if col < shape[1] - 1 and not graph[row][col+1].processed:
             neighbors.append(graph[row][col+1])
     return neighbors
@@ -38,7 +38,7 @@ def bubble_up(queue, ind):
             queue = bubble_up(queue, pInd)
     return queue
     
-def bubble_down(queue, index):
+def bubble_down(queue, ind):
 
     length=len(queue)
     lcInd = 2 * ind + 1
@@ -48,23 +48,28 @@ def bubble_down(queue, index):
     if lcInd < length and rcInd >= length: #just left child
         if queue[ind].distFromSrc > queue[lcInd].distFromSrc:
             queue[ind], queue[lcInd] = queue [lcInd], queue[ind]
-            queue[ind].index_in_queue=index
+            queue[ind].index_in_queue=ind
             queue[lcInd].index_in_queue = lcInd
             queue = bubble_down(queue, lcInd)
     else:
         small = lcInd
-        if queue[lcInd].d > queue[rcInd].d:
+        if queue[lcInd].distFromSrc > queue[rcInd].distFromSrc:
             small = rcInd
-        if queue[small].d < queue[ind].d:
+        if queue[small].distFromSrc < queue[ind].distFromSrc:
             queue[ind],queue[small]=queue[small],queue[ind]
-            queue[ind].index_in_queue=index
+            queue[ind].index_in_queue=ind
             queue[small].index_in_queue=small
             queue = bubble_down(queue, small)
     return queue
 
 # Get distance using RGB
+# u, v as (x, y)
 def get_distance(img,u,v):
-    return 0.1 + (float(img[v][0])-float(img[u][0]))**2+(float(img[v][1])-float(img[u][1]))**2+(float(img[v][2])-float(img[u][2]))**2
+    img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    value_u = img[u[0]][u[1]]
+    value_v = img[v[0]][v[1]]
+    img.setflags(write=True)
+    return 0.1 + float(value_v[0]-value_u[0])**2+float(value_v[1]-value_u[1])**2+float(value_v[2]-value_u[2])**2
 
 def drawPath(img,path):
     # Path is list of tuples containing x, y coords
@@ -94,14 +99,17 @@ def find_shortest_path(img,src,dst):
 
     while len(prioQueue) > 0:
         vert = prioQueue[0]
-        vert.processed=True
+        
         prioQueue[0]=prioQueue[-1]
         prioQueue[0].index_in_queue=0
         prioQueue.pop()
         prioQueue=bubble_down(prioQueue,0)
+
+        vert.processed=True
+
         neighbors = get_neighbors(matrix,vert.y, vert.x)
         for i in neighbors:
-            dist = get_distance(img,(vert.y, vert.x),(i.y, i.x))
+            dist = get_distance(img,(vert.x, vert.y),(i.x, i.y))
             if vert.distFromSrc + dist < i.distFromSrc:
                 i.distFromSrc = vert.distFromSrc + dist
                 i.xParent = vert.x
@@ -121,9 +129,11 @@ def find_shortest_path(img,src,dst):
     return path
 
 
-image = cv.imread('maze_test.pgm')
-cv.convert_to_grayscale(image)
-
-path = find_shortest_path(img,STARTNODE,ENDNODE)
+image = cv2.imread('maze_test.pgm')
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+h, w, d = image.shape
+STARTNODE = [291, 101]
+ENDNODE = [132, 305]
+path = find_shortest_path(gray,STARTNODE,ENDNODE)
 
 drawPath(image,path)
